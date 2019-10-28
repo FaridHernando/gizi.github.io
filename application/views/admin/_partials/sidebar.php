6468ccd9778be9ef5d31d362e18c507d7253fefd
Maming12 <mamingseriez@gmail.com>
2019-10-28 21:27:16 +0700

    Sidebar

--- application/views/admin/_partials/sidebar.php
+++ application/views/admin/_partials/sidebar.php
@@ -3,28 +3,28 @@
     <li class="nav-item <?php echo $this->uri->segment(2) == '' ? 'active': '' ?>">
         <a class="nav-link" href="<?php echo site_url('admin') ?>">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Overview</span>
+            <span>Dashboard</span>
         </a>
     </li>
     <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'Balita' ? 'active': '' ?>">
         <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
             aria-expanded="false">
             <i class="fas fa-fw fa-boxes"></i>
-            <span>Balita</span>
+            <span>Balita Kita</span>
         </a>
         <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-            <a class="dropdown-item" href="<?php echo site_url('admin/sbadmin/add') ?>">New Balita</a>
-            <a class="dropdown-item" href="<?php echo site_url('admin/newsbadmin') ?>">List Balita</a>
+            <a class="dropdown-item" href="<?php echo site_url('admin/sbadmin/add') ?>">Tambah Balita</a>
+            <a class="dropdown-item" href="<?php echo site_url('admin/newsbadmin') ?>">Daftar Balita</a>
         </div>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="#">
             <i class="fas fa-fw fa-users"></i>
-            <span>admin</span></a>
+            <span>User</span></a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="#">
             <i class="fas fa-fw fa-cog"></i>
-            <span>extend</span></a>
+            <span>Pengaturan</span></a>
     </li>
 </ul>
