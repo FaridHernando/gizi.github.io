9131b823b97b056b3aa44467cf582b48628e92a5
Maming12 <mamingseriez@gmail.com>
2019-10-28 21:33:17 +0700

    navbar

--- application/views/admin/_partials/navbar.php
+++ application/views/admin/_partials/navbar.php
@@ -9,7 +9,7 @@
     <!-- Navbar Search -->
     <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
         <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+            <input type="text" class="form-control" placeholder="Pencarian" aria-label="Search" aria-describedby="basic-addon2">
             <div class="input-group-append">
                 <button class="btn btn-light" type="button">
                     <i class="fas fa-search"></i>
@@ -27,11 +27,8 @@
                 <span class="badge badge-danger">9+</span>
             </a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-            </div>
+                <a class="dropdown-item" href="#">Notifikasi</a>
+                
         </li>
 
         <li class="nav-item dropdown no-arrow mx-1">
@@ -41,11 +38,8 @@
                 <span class="badge badge-danger">7</span>
             </a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-            </div>
+                <a class="dropdown-item" href="#">Pesan Masuk</a>
+                
         </li>
 
         <li class="nav-item dropdown no-arrow">
