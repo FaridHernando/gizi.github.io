f40d39cd2f9fe9d480965f06a50014a1146b943b
sigmaluxvian-161240000526 <56667404+sigmaluxvian-161240000526@users.noreply.github.com>
2019-10-28 15:32:05 +0700

    tes

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,7 +19,7 @@ class Welcome extends CI_Controller {
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 
-	//tambah jajal saaaaaaa
+	//tambah jajal saaaaaaa berrr
 	public function index()
 	{
 		$this->load->view('welcome_message');
